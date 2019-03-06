f5c51ce76663d5f38644563f499fb8cfb4751c8f
Chacal <quentin1.thomas@epitech.eu>
2019-03-06 01:00:00 +0100

    adding basic visuel

---
+++ electron/renderer.js
@@ -0,0 +1,9 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+var $ = require("jquery");
+
+    var blbl = $("#blbl").click(function () {
+       $("#blbl").text("BITR")
+    });
