4806bff83286c80aa3d9323be18ab0737a57961a
root <quentin1.thomas@epitech.eu>
2019-03-12 18:58:55 +0100

    fixing visual

--- electron/main.js
+++ electron/main.js
@@ -14,6 +14,7 @@ function createWindow () {
       nodeIntegration: true
     }
   })
+  mainWindow.setResizable(false)
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
