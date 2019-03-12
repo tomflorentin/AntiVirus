31fe49a02afc67520ecdf7b6ed101fa28ea59f35
root <quentin1.thomas@epitech.eu>
2019-03-12 00:51:06 +0000

    basic visual (omg je j'ai travailler)

--- electron/main.js
+++ electron/main.js
@@ -8,8 +8,8 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     webPreferences: {
       nodeIntegration: true
     }
